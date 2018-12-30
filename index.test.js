6f7cbc2216d03b83d5a1003c617e268e3a1c7f41
Chris Deacy <chrisdothtml@gmail.com>
2018-12-30 12:17:15 -0800

    adding more test assertions

--- index.test.js
+++ index.test.js
@@ -4,6 +4,16 @@ import minimatch from './index.js'
 test({
   name: 'minimatch',
   fn () {
+    const mmInstance = new minimatch.Minimatch('*.js')
+
     assertEqual(minimatch('bar.foo', '*.foo'), true)
+    assertEqual(minimatch('bar.foo', '*.+(bar|foo)'), true)
+    assertEqual(minimatch('bar.foo', '*.bar'), false)
+    assertEqual(
+      ['foo.js', 'foo.txt'].filter(minimatch.filter('*.js')),
+      ['foo.js']
+    )
+    assertEqual(mmInstance.match('foo.js'), true)
+    assertEqual(mmInstance.makeRe() instanceof RegExp, true)
   }
 })
