1cbaa25a3463cf5cf1edbd2d326663538d68310c
Chris Deacy <chrisdothtml@gmail.com>
2019-06-18 20:23:35 -0700

    build out script

--- gh-actions/rebuild/rebuild.js
+++ gh-actions/rebuild/rebuild.js
@@ -1,26 +1,54 @@
 const {
+  // GITHUB_EVENT_NAME,
   GITHUB_EVENT_PATH,
   GITHUB_REF,
   GITHUB_REPOSITORY,
   GITHUB_TOKEN,
 } = process.env
+
 const { execSync } = require('child_process')
 const githubEvent = require(GITHUB_EVENT_PATH)
+const COMMIT_MESSAGE = '[gh-actions/rebuild] Rebuild index.js'
 
 function exec(cmd) {
   return execSync(cmd, { encoding: 'utf-8' }).replace(/\n$/, '')
 }
 
-// if (GITHUB_REF === 'refs/heads/master') {
+function getChangedFiles() {
+  return exec(`git status --porcelain`)
+    .split('\n')
+    .map((line) => line.slice(3))
+}
+
+// if (
+//   GITHUB_EVENT_NAME === 'push' &&
+//   GITHUB_REF === 'refs/heads/master' &&
+//   githubEvent.commits[0].message !== COMMIT_MESSAGE
+// ) {
 const remoteUrl = `https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`
 const branchName = GITHUB_REF.split('/')[2]
 
-console.log('githubEvent')
-console.log(require('util').inspect(githubEvent, false, null, true))
-console.log('remoteUrl')
-console.log(remoteUrl)
-console.log('branchName')
-console.log(branchName)
-console.log('git user')
-console.log(exec(`git config --get user.name`))
+exec(`yarn build`)
+if (getChangedFiles().includes('index.js')) {
+  exec(
+    [
+      // configure git
+      `git config user.email "${githubEvent.head_commit.author.email}"`,
+      `git config user.name "${githubEvent.head_commit.author.name}"`,
+      `git remote add gh-action ${remoteUrl}`,
+      `git fetch gh-action ${branchName}`,
+      `git branch --set-upstream-to gh-action/${branchName}`,
+      // push change
+      `git add index.js`,
+      `git commit -m "${COMMIT_MESSAGE}"`,
+      `git push`,
+    ].join(' && ')
+  )
+
+  // success
+  process.exit(0)
+}
 // }
+
+// neutral
+process.exit(78)
