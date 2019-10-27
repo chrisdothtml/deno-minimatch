e0cbe0d9e96f1fd2254560dab32069a9f2dd3fd7
Chris Deacy <chrisdothtml@gmail.com>
2019-10-27 11:46:25 -0700

    Update and rename gh-actions/rebuild/rebuild.js to gh-actions/rebuild.js

---
+++ gh-actions/rebuild.js
@@ -0,0 +1,47 @@
+const {
+  AUTHOR_EMAIL,
+  AUTHOR_NAME,
+  GITHUB_TOKEN,
+  REF,
+  REPO,
+} = process.env
+
+const { execSync } = require('child_process')
+const remoteUrl = `https://x-access-token:${GITHUB_TOKEN}@github.com/${REPO}.git`
+const branchName = REF.split('/')[2]
+
+function exec(cmd) {
+  return execSync(cmd, { encoding: 'utf-8' }).replace(/\n$/, '')
+}
+
+function getChangedFiles() {
+  return exec(`git status --porcelain`)
+    .split('\n')
+    .map((line) => line.slice(3))
+}
+
+exec(`yarn && yarn build`)
+if (getChangedFiles().includes('index.js')) {
+  exec(
+    [
+      // configure git
+      `git config user.email "${AUTHOR_EMAIL}"`,
+      `git config user.name "${AUTHOR_NAME}"`,
+      `git remote add gh-action ${remoteUrl}`,
+      `git fetch gh-action ${branchName}`,
+      `git branch --set-upstream-to gh-action/${branchName}`,
+      // push change
+      `git add index.js`,
+      `git commit -m "[gh-actions/rebuild] Rebuild index.js"`,
+      `git push`,
+    ].join(' && ')
+  )
+
+  // success
+  process.exit(0)
+} else {
+  console.log('Dist file not changed')
+}
+
+// neutral
+process.exit(78)
