d350b3f982a2bcb895d86951df5a18a724f0ef77
Chris Deacy <chrisdothtml@gmail.com>
2020-02-26 19:22:16 -0800

    Fix test import

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { runTests, test } from 'https://deno.land/std/testing/mod.ts'
+import { runTests, test } from Deno
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts'
 import minimatch from './index.js'
 
